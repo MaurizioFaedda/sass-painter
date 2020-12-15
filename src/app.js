192e208a0def32be70eca5b4ae79e16b2ef2680b
Maurizio <maurizio.faedda1@gmail.com>
2020-12-16 00:09:42 +0100

    dynamic footer menu with vuejs

---
+++ src/app.js
@@ -0,0 +1,114 @@
+var app = new Vue ({
+    el: '#root',
+    data:{
+        footer_menu: {
+            'Products': [
+                {
+                    title: "Corel Painter",
+                    link: "https://www.painterartist.com/en/product/painter/?ft",
+
+                },
+                {
+                    title: "ParticleShop",
+                    link: "https://www.painterartist.com/en/product/particleshop/?ft",
+                },
+                {
+                    title: "Painter Essentials",
+                    link: "https://www.painterartist.com/en/product/painter-essentials/?ft",
+
+                },
+                {
+                    title: "Special Offers",
+                    link: "https://www.painterartist.com/en/special-offers/?ft",
+                },
+                {
+                    title: "Free Trials",
+                    link: "https://www.painterartist.com/en/free-trials/?ft",
+                },
+                {
+                    title: "All products",
+                    link: "https://www.painterartist.com/en/all-products/?ft",
+                }
+            ],
+
+            'Quick Links': [
+                {
+                    title: "Licensing",
+                    link: "https://www.painterartist.com/en/licensing/?ft",
+                },
+                {
+                    title: "Tutorials & Tips",
+                    link: "https://www.painterartist.com/en/pages/tutorials/?ft",
+                },
+                {
+                    title: "Webinars",
+                    link: "https://www.painterartist.com/en/pages/webinars/?ft",
+                },
+                {
+                    title:"The Painter Factory",
+                    link: "http://painterfactory.com/",
+                },
+                {
+                    title:"Discovery Center",
+                    link: "https://learn.corel.com/graphics/home",
+                },
+                {
+                    title: "Beta Programs",
+                    link: "https://www.corel.com/beta/",
+                },
+            ],
+
+            'Support': [
+                {
+                    title: "Register your product",
+                    link: "http://account.corel.com/accounts/user/registerProduct?sso-brandId=painter",
+                },
+                {
+                    title:"Patches & Updates",
+                    link: "https://www.painterartist.com/en/support/updates/?ft",
+                },
+                {
+                    title: "Knowledgebase",
+                    link: "https://support.corel.com/hc/en-us/sections/360002999034-Painter",
+                },
+                {
+                    title: "Uninstall instructions",
+                    link: "https://support.corel.com/hc/en-us/articles/215822078",
+                },
+                {
+                    title: "Contact us",
+                    link: "https://www.corel.com/contact/",
+                },
+
+            ],
+            'About Corel': [
+                {
+                    title: "Company information",
+                    link: "https://www.corel.com/our-story/",
+                },
+                {
+                    title: "Newsroom",
+                    link: "https://www.corel.com/newsroom/",
+                },
+                {
+                    title: "Careers",
+                    link: "https://www.corel.com/careers/",
+                },
+                {
+                    title: "Partner Program",
+                    link: "https://www.corel.com/partners/",
+                },
+                {
+                    title: "Legal Information",
+                    link: "https://www.corel.com/legal/",
+                },
+                {
+                    title: "EULA",
+                    link: "https://www.corel.com/eula/",
+                },
+
+            ],
+
+        }
+    }
+})
